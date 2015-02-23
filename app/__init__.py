e292cdee6f25f7784a8b92aa25e92c488be6bdb3
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-02-23 14:48:12 +0000

    Skeleton "Hello, Nicole!" app.

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+
+
+bootstrap = Bootstrap()
+
+
+def create_app(config_name):
+
+    application = Flask(__name__)
+    application.config['DEBUG'] = True
+    
+    bootstrap.init_app(application)
+
+    from .main import main as main_blueprint
+    application.register_blueprint(main_blueprint)
+
+    return application
