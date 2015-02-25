0fcdb88358c0f08fd65716bbfd6c4092e1a61bfb
Chris Hill-Scott <me@quis.cc>
2015-02-25 17:07:31 +0000

    Add template to other pages, use toolkit
    Sorry for the size of this single commit.
    
    It does three main things:
    - Adds the template to the remaining pages
    - Makes all elements of the page use elements from the Digital
      Marketplace frontend toolkit
    - Prototype how the flow from page to page might work (without
      adding any new functionality, ie the app still doesn't _do_
      anything)

--- app/main/views.py
+++ app/main/views.py
@@ -1,38 +1,56 @@
 import requests
-import os
+import os, re
 from . import main
-from flask import json, render_template, Response, request
+from flask import json, render_template, Response, request, redirect
 
 
 @main.route('/')
 def index():
     template_data = main.config['BASE_TEMPLATE_DATA']
-    return render_template("index.html", **template_data), 200
+    return render_template("index.html", **template_data)
 
+@main.route('/service')
+def find_service():
+    service_id = request.args.get("service_id")
+    return redirect("/service/" + service_id)
 
-@main.route('/editservice')
-def get_service():
+@main.route('/service/<service_id>')
+def view_service(service_id):
     template_data = main.config['BASE_TEMPLATE_DATA']
     try:
-        service_id = request.args.get("service_id")
         service_json = json.loads(get_service_json(service_id))["services"]
         template_data["service_data"] = service_json
-        return render_template(
-            "edit_service.html", **template_data), 200
-    except KeyError:
-        return Response("Service ID '%s' can not be found" % service_id, 404)
-
-
-@main.route('/viewservice')
-def get_service_by_id():
-    template_data = main.config['BASE_TEMPLATE_DATA']
-    try:
-        service_id = request.args.get("service_id")
-        service_json = json.loads(get_service_json(service_id))["services"]
-        template_data["service_data"] = service_json
+        template_data["service_data"]["id_split"] = re.findall("....", str(template_data["service_data"]["id"]))
         return render_template("view_service.html", **template_data)
     except KeyError:
         return Response("Service ID '%s' can not be found" % service_id, 404)
+
+@main.route('/service/<service_id>/raw')
+def view_service_raw(service_id):
+    try:
+        service_json = json.loads(get_service_json(service_id))["services"]
+        return Response(json.dumps(service_json), mimetype='application/json')
+    except KeyError:
+        return Response("Service ID '%s' can not be found" % service_id, 404)
+
+@main.route('/service/<service_id>/edit/documents')
+def edit_documents(service_id):
+    template_data = main.config['BASE_TEMPLATE_DATA']
+    try:
+        service_json = json.loads(get_service_json(service_id))["services"]
+        template_data["service_data"] = service_json
+        return render_template(
+            "edit_documents.html", **template_data), 200
+    except KeyError:
+        return Response("Service ID '%s' can not be found" % service_id, 404)
+
+@main.route('/service/<service_id>', methods=['POST'])
+def update(service_id):
+    template_data = main.config['BASE_TEMPLATE_DATA']
+    template_data["edits_submitted"] = request.form.getlist("edits_submitted")
+    print "===================="
+    print template_data["edits_submitted"]
+    return render_template("confirm.html", **template_data), 200
 
 
 def get_service_json(service_id):
