a430a5c6c8f88579e6ba2de492ba3e592ab0423c
Chris Hill-Scott <me@quis.cc>
2015-03-06 15:48:34 +0000

    Require authentication on all views
    Move authentication to a before_request filter rather than a decorator.
    We require authentication on all views so requiring it to be added with
    a decorator to each view just makes it easier to forget.

--- app/main/helpers/auth.py
+++ app/main/helpers/auth.py
@@ -22,11 +22,7 @@ def authenticate():
     )
 
 
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
+def requires_auth():
+    auth = request.authorization
+    if not auth or not check_auth(auth.username, auth.password):
+        return authenticate()
