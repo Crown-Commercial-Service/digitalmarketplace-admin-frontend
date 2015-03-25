e0441de7de9cdf725cb68773d6da06173668fd19
Alexey Bezhan <al@fueledbylemons.com>
2015-03-25 16:57:49 +0000

    Add set_question helper for validation_tools tests

--- tests/app/main/helpers/test_validation_tools.py
+++ tests/app/main/helpers/test_validation_tools.py
@@ -23,70 +23,70 @@ class TestValidate(unittest.TestCase):
 
         self.validate.content.get_question = lambda key: self.content[key]
 
+    def set_question(self, question_id, data, *validations, **kwargs):
+        self.data[question_id] = data
+        if 'value' in kwargs:
+            self.service[question_id] = kwargs['value']
+        self.content[question_id] = {'validations': validations}
+
     def test_validate_empty(self):
         self.assertEquals(self.validate.errors, {})
 
     def test_validate_empty_question(self):
-        self.data['q1'] = mock_file('a', 0)
-        self.content['q1'] = {'validations': []}
+        self.set_question('q1', mock_file('a', 0))
         self.assertEquals(self.validate.errors, {})
 
     def test_validate_non_empty_answer_required(self):
-        self.data['q1'] = mock_file('a', 1)
-        self.content['q1'] = {
-            'validations': [
-                {'name': 'answer_required', 'message': 'failed'}
-            ]
-        }
+        self.set_question(
+            'q1', mock_file('a', 1),
+            {'name': 'answer_required', 'message': 'failed'}
+        )
         self.assertEquals(self.validate.errors, {})
 
     def test_validate_empty_answer_required(self):
-        self.data['q1'] = mock_file('a.pdf', 0)
-        self.content['q1'] = {
-            'validations': [
-                {'name': 'answer_required', 'message': 'failed'}
-            ]
-        }
+        self.set_question(
+            'q1', mock_file('a.pdf', 0),
+            {'name': 'answer_required', 'message': 'failed'}
+        )
         self.assertEquals(self.validate.errors, {'q1': 'failed'})
 
+    def test_validate_empty_answer_required_previous_value(self):
+        self.set_question(
+            'q1', mock_file('a.pdf', 0),
+            {'name': 'answer_required', 'message': 'failed'},
+            value='b.pdf',
+        )
+        self.assertEquals(self.validate.errors, {})
+
     def test_incorrect_document_format(self):
-        self.data['q1'] = mock_file('a.txt', 1)
-        self.content['q1'] = {
-            'validations': [
-                {'name': 'file_is_open_document_format', 'message': 'failed'}
-            ]
-        }
+        self.set_question(
+            'q1', mock_file('a.txt', 1),
+            {'name': 'file_is_open_document_format', 'message': 'failed'}
+        )
         self.assertEquals(self.validate.errors, {'q1': 'failed'})
 
     def test_incorrect_file_size(self):
-        self.data['q1'] = mock_file('a.pdf', 5400001)
-        self.content['q1'] = {
-            'validations': [
-                {'name': 'file_is_less_than_5mb', 'message': 'failed'}
-            ]
-        }
+        self.set_question(
+            'q1', mock_file('a.pdf', 5400001),
+            {'name': 'file_is_less_than_5mb', 'message': 'failed'}
+        )
         self.assertEquals(self.validate.errors, {'q1': 'failed'})
 
     def test_list_of_validations(self):
-        self.data['q1'] = mock_file('a.pdf', 1)
-        self.service['q1'] = 'b.pdf'
-        self.content['q1'] = {
-            'validations': [
-                {'name': 'file_is_open_document_format', 'message': 'failed'},
-                {'name': 'file_is_less_than_5mb', 'message': 'failed'},
-            ]
-        }
+        self.set_question(
+            'q1', mock_file('a.pdf', 1),
+            {'name': 'file_is_open_document_format', 'message': 'failed'},
+            {'name': 'file_is_less_than_5mb', 'message': 'failed'},
+            value='b.pdf'
+        )
         self.assertEquals(self.validate.errors, {})
 
     def test_file_save(self):
-        self.data['pricingDocumentURL'] = mock_file('a.pdf', 1,
-                                                    'pricingDocumentURL')
-        self.service['pricingDocumentURL'] = 'b.pdf'
-        self.content['pricingDocumentURL'] = {
-            'validations': [
-                {'name': 'file_can_be_saved', 'message': 'failed'},
-            ]
-        }
+        self.set_question(
+            'pricingDocumentURL', mock_file('a.pdf', 1, 'pricingDocumentURL'),
+            {'name': 'file_can_be_saved', 'message': 'failed'},
+            value='b.pdf'
+        )
 
         self.assertEquals(self.validate.errors, {})
         self.uploader.save.assert_called_once_with(
@@ -99,29 +99,31 @@ class TestValidate(unittest.TestCase):
             'pricingDocumentURL': 'https://assets.test.digitalmarketplace.service.gov.uk/documents/2/1-pricing-document.pdf',  # noqa
         })
 
+    def test_field_with_no_previous_value(self):
+        self.set_question(
+            'pricingDocumentURL', mock_file('a.pdf', 1),
+            {'name': 'answer_required', 'message': 'failed'},
+            {'name': 'file_is_open_document_format', 'message': 'failed'},
+            {'name': 'file_is_less_than_5mb', 'message': 'failed'},
+            {'name': 'file_can_be_saved', 'message': 'failed'},
+            value=None
+        )
+        self.assertEquals(self.validate.errors, {})
+
     def test_multiple_fields_list_of_validations(self):
-        self.data.update({
-            'q0': mock_file('a.pdf', 1),
-            'q1': mock_file('a.txt', 1),
-            'q2': mock_file('a.pdf', 5400001),
-            'q3': mock_file('a.txt', 5400001),
-        })
-        self.service.update({
-            'q0': 'b.pdf',
-            'q1': 'b.pdf',
-            'q2': 'b.pdf',
-            'q3': 'b.pdf',
-        })
         validations = [
             {'name': 'file_is_open_document_format', 'message': 'format'},
             {'name': 'file_is_less_than_5mb', 'message': 'size'},
         ]
-        self.content.update({
-            'q0': {'validations': validations},
-            'q1': {'validations': validations},
-            'q2': {'validations': validations},
-            'q3': {'validations': validations},
-        })
+
+        self.set_question('q0', mock_file('a.pdf', 1),
+                          value='b.pdf', *validations)
+        self.set_question('q1', mock_file('a.txt', 1),
+                          value='b.pdf', *validations)
+        self.set_question('q2', mock_file('a.pdf', 5400001),
+                          value='b.pdf', *validations)
+        self.set_question('q3', mock_file('a.txt', 5400001),
+                          value='b.pdf', *validations)
 
         self.assertEquals(self.validate.errors, {
             'q1': 'format',
