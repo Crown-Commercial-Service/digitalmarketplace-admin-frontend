c313d6fb6803edabb956e1e90f040f8518c334bf
Alexey Bezhan <al@fueledbylemons.com>
2015-04-17 11:43:53 +0100

    Fix 404 page template static resources

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,4 +4,5 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template("404.html",
+                           **main.config['BASE_TEMPLATE_DATA']), 404
