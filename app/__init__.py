4bb5b356ad1c86122f56ecb92fd19ce7061f5582
Alexey Bezhan <al@fueledbylemons.com>
2015-04-17 12:09:28 +0100

    Add status blueprint with /_status endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.bootstrap import Bootstrap
 from config import config
 from datetime import timedelta
 from .main import main as main_blueprint
+from .status import status as status_blueprint
 from .main.helpers.auth import requires_auth
 
 bootstrap = Bootstrap()
@@ -23,6 +24,7 @@ def create_app(config_name):
         application.permanent_session_lifetime = timedelta(minutes=60)
         main_blueprint.before_request(requires_auth)
 
+    application.register_blueprint(status_blueprint)
     application.register_blueprint(main_blueprint, url_prefix='/admin')
     main_blueprint.config = application.config.copy()
 
