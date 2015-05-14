4b556c64aa3bbcc14e221bf89ac5538f5194809d
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-05-14 16:37:54 +0100

    Update tests to use the new /services/ routes

--- tests/app/main/test_views.py
+++ tests/app/main/test_views.py
@@ -81,7 +81,7 @@ class TestServiceView(LoggedInApplicationTest):
         error.response.status_code = 404
         data_api_client.get_service.side_effect = APIError(error)
 
-        response1 = self.client.get('/admin/service/1')
+        response1 = self.client.get('/admin/services/1')
         self.assertEquals(302, response1.status_code)
         self.assertEquals(response1.location, 'http://localhost/admin/')
         response2 = self.client.get(response1.location)
@@ -245,7 +245,7 @@ class TestServiceStatusUpdate(LoggedInApplicationTest):
             'supplierId': 2,
             'status': 'disabled'
         }}
-        response = self.client.get('/admin/service/1')
+        response = self.client.get('/admin/services/1')
         self.assertIn('<input type="radio" name="service_status" id="service_status_disabled" value="removed" checked="checked" />', response.data)  # noqa
         self.assertIn('<input type="radio" name="service_status" id="service_status_private" value="private"  />', response.data)  # noqa
         self.assertNotIn('<input type="radio" name="service_status" id="service_status_published" value="public"  />', response.data)  # noqa
@@ -257,7 +257,7 @@ class TestServiceStatusUpdate(LoggedInApplicationTest):
             'supplierId': 2,
             'status': 'enabled'
         }}
-        response = self.client.get('/admin/service/1')
+        response = self.client.get('/admin/services/1')
         self.assertIn('<input type="radio" name="service_status" id="service_status_disabled" value="removed"  />', response.data)  # noqa
         self.assertIn('<input type="radio" name="service_status" id="service_status_private" value="private" checked="checked" />', response.data)  # noqa
         self.assertIn('<input type="radio" name="service_status" id="service_status_published" value="public"  />', response.data)  # noqa
@@ -269,7 +269,7 @@ class TestServiceStatusUpdate(LoggedInApplicationTest):
             'supplierId': 2,
             'status': 'published'
         }}
-        response = self.client.get('/admin/service/1')
+        response = self.client.get('/admin/services/1')
         self.assertIn('<input type="radio" name="service_status" id="service_status_disabled" value="removed"  />', response.data)  # noqa
         self.assertIn('<input type="radio" name="service_status" id="service_status_private" value="private"  />', response.data)  # noqa
         self.assertIn('<input type="radio" name="service_status" id="service_status_published" value="public" checked="checked" />', response.data)  # noqa
@@ -277,14 +277,14 @@ class TestServiceStatusUpdate(LoggedInApplicationTest):
     @mock.patch('app.main.views.data_api_client')
     def test_status_update_to_removed(self, data_api_client):
         data_api_client.get_service.return_value = {'services': {}}
-        response1 = self.client.post('/admin/service/status/1',
+        response1 = self.client.post('/admin/services/status/1',
                                      data={'service_status': 'removed'})
         data_api_client.update_service_status.assert_called_with(
             '1', 'disabled', 'Digital Marketplace admin user',
             "Status changed to 'disabled'")
         self.assertEquals(302, response1.status_code)
         self.assertEquals(response1.location,
-                          'http://localhost/admin/service/1')
+                          'http://localhost/admin/services/1')
         response2 = self.client.get(response1.location)
         self.assertIn("Service status has been updated to: Removed",
                       response2.data)
@@ -292,14 +292,14 @@ class TestServiceStatusUpdate(LoggedInApplicationTest):
     @mock.patch('app.main.views.data_api_client')
     def test_status_update_to_private(self, data_api_client):
         data_api_client.get_service.return_value = {'services': {}}
-        response1 = self.client.post('/admin/service/status/1',
+        response1 = self.client.post('/admin/services/status/1',
                                      data={'service_status': 'private'})
         data_api_client.update_service_status.assert_called_with(
             '1', 'enabled', 'Digital Marketplace admin user',
             "Status changed to 'enabled'")
         self.assertEquals(302, response1.status_code)
         self.assertEquals(response1.location,
-                          'http://localhost/admin/service/1')
+                          'http://localhost/admin/services/1')
         response2 = self.client.get(response1.location)
         self.assertIn("Service status has been updated to: Private",
                       response2.data)
@@ -307,25 +307,25 @@ class TestServiceStatusUpdate(LoggedInApplicationTest):
     @mock.patch('app.main.views.data_api_client')
     def test_status_update_to_published(self, data_api_client):
         data_api_client.get_service.return_value = {'services': {}}
-        response1 = self.client.post('/admin/service/status/1',
+        response1 = self.client.post('/admin/services/status/1',
                                      data={'service_status': 'public'})
         data_api_client.update_service_status.assert_called_with(
             '1', 'published', 'Digital Marketplace admin user',
             "Status changed to 'published'")
         self.assertEquals(302, response1.status_code)
         self.assertEquals(response1.location,
-                          'http://localhost/admin/service/1')
+                          'http://localhost/admin/services/1')
         response2 = self.client.get(response1.location)
         self.assertIn("Service status has been updated to: Public",
                       response2.data)
 
     @mock.patch('app.main.views.data_api_client')
     def test_bad_status_gives_error_message(self, data_api_client):
-        response1 = self.client.post('/admin/service/status/1',
+        response1 = self.client.post('/admin/services/status/1',
                                      data={'service_status': 'suspended'})
         self.assertEquals(302, response1.status_code)
         self.assertEquals(response1.location,
-                          'http://localhost/admin/service/1')
+                          'http://localhost/admin/services/1')
         response2 = self.client.get(response1.location)
         self.assertIn("Not a valid status: 'suspended'",
                       response2.data)
