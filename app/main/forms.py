23cbe17807a4c64314648b822b1939c1ad7b75b3
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-06-24 10:31:22 +0100

    updated validator

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, DateField
-from wtforms.validators import DataRequired, Optional, Regexp, AnyOf
+from wtforms.validators import Optional, AnyOf
 from datetime import datetime
 
 
