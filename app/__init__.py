7b903ee5be175cffa2d52257b050bb63d0557aac
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-07-02 12:53:54 +0100

    Log user out if user account is deleted
    If a user account that is in the cookie no longer exists in the API
    then log the user out.

--- app/__init__.py
+++ app/__init__.py
@@ -72,4 +72,6 @@ def create_app(config_name):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.from_json(data_api_client.get_user(user_id=int(user_id)))
+    user_json = data_api_client.get_user(user_id=int(user_id))
+    if user_json:
+        return User.from_json(user_json)
