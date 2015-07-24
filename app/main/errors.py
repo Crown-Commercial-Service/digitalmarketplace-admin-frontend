b9cd6091527cc181da2d62e46cfce281cea03e91
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-07-24 16:36:14 +0100

    Error case for 400 bad requests

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,6 +2,11 @@ from flask import render_template
 from . import main
 
 
+@main.app_errorhandler(400)
+def page_not_found(e):
+    return render_template("errors/500.html",
+                           **main.config['BASE_TEMPLATE_DATA']), 404
+
 @main.app_errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html",
