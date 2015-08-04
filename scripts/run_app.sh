b0bf93824d822dcfaaf65b44348d885d6c528002
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-08-04 11:18:46 +0100

    Added flash messages and default params for start script

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -11,6 +11,8 @@ export DM_DATA_API_AUTH_TOKEN=${DM_DATA_API_AUTH_TOKEN:=myToken}
 # Default hash for user/pass combo: admin/admin
 export DM_ADMIN_FRONTEND_COOKIE_SECRET=${DM_ADMIN_FRONTEND_COOKIE_SECRET:=secret}
 export DM_S3_DOCUMENT_BUCKET=${DM_S3_DOCUMENT_BUCKET:=admin-frontend-dev-documents}
+export DM_MANDRILL_API_KEY=${DM_MANDRILL_API_KEY:=not_a_real_key}
+export DM_PASSWORD_SECRET_KEY=${DM_PASSWORD_SECRET_KEY:=verySecretKey}
 
 echo "Environment variables in use:"
 env | grep DM_
