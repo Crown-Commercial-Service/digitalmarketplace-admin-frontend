a687b0bef72e66c856217341f8f71dd3721276e8
Chris Hill-Scott <me@quis.cc>
2015-10-22 17:59:44 +0100

    Watch for changes to content
    Implements https://github.com/alphagov/digitalmarketplace-utils/pull/193
    
    Adds https://pypi.python.org/pypi/watchdog as a requirement to make watching
    for file changes use system events, which is faster

--- application.py
+++ application.py
@@ -3,12 +3,13 @@
 import os
 from app import create_app
 from flask.ext.script import Manager, Server
+from dmutils import init_manager
 
 application = create_app(
     os.getenv('DM_ENVIRONMENT') or 'development'
 )
 manager = Manager(application)
-manager.add_command("runserver", Server(port=5004))
+init_manager(manager, 5004, ['./app/content/frameworks'])
 
 if __name__ == '__main__':
     manager.run()
