fc6d80363e02314f0502cab920995582774c6302
Chris Hill-Scott <me@quis.cc>
2015-10-27 09:42:58 +0000

    Have flask init init the manager
    Depends on and implements https://github.com/alphagov/digitalmarketplace-utils/pull/195

--- application.py
+++ application.py
@@ -2,14 +2,12 @@
 
 import os
 from app import create_app
-from flask.ext.script import Manager, Server
 from dmutils import init_manager
 
 application = create_app(
     os.getenv('DM_ENVIRONMENT') or 'development'
 )
-manager = Manager(application)
-init_manager(manager, 5004, ['./app/content/frameworks'])
+init_manager(application, 5004, ['./app/content/frameworks'])
 
 if __name__ == '__main__':
     manager.run()
