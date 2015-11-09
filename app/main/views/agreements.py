d383b5fa5c747290b3d40cda4b8f2195efcabc44
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-11-09 18:09:36 +0000

    Format agreement upload date

--- app/main/views/agreements.py
+++ app/main/views/agreements.py
@@ -1,5 +1,8 @@
 from flask import render_template
 from flask_login import login_required
+from dateutil.parser import parse as parse_date
+
+from dmutils.formats import datetimeformat
 
 from .. import main
 from ..auth import role_required
@@ -15,6 +18,10 @@ def list_agreements(framework_slug):
     supplier_frameworks = data_api_client.find_framework_suppliers(
         framework_slug, agreement_returned=True
     )['supplierFrameworks']
+
+    for supplier_framework in supplier_frameworks:
+        supplier_framework['agreementReturnedAt'] = datetimeformat(
+            parse_date(supplier_framework['agreementReturnedAt']))
 
     return render_template(
         'view_agreements.html',
