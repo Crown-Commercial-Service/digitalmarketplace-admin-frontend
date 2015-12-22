86fabab81175a964836948692224c088f6c1547c
Paul Craig <paul_craig_16@hotmail.com>
2015-12-22 15:27:11 +0000

    Add route and template for exporting users
    Added a new page where an admin user can select from any frameworks
    that aren't 'coming' or 'expired' and download a list of their users
    as a csv.
    Copy isn't yet finalised.

--- app/main/views/users.py
+++ app/main/views/users.py
@@ -31,3 +31,58 @@ def find_user_by_email_address():
             users=list(),
             email_address=None,
             **get_template_data()), 404
+
+
+@main.route('/users/export', methods=['GET', 'POST'])
+@login_required
+@role_required('admin')
+def export_users():
+    bad_statuses = ['coming', 'expired']
+    frameworks = [f for f in data_api_client.find_frameworks()['frameworks'] if f['status'] not in bad_statuses]
+    framework_options = [{'value': f['slug'], 'label': f['name']} for f in sorted(frameworks, key=lambda f: f['name'])]
+    errors = []
+
+    if request.method == 'POST':
+
+        framework_slug = request.form.get('framework_slug')
+
+        if not framework_slug or framework_slug not in [f['slug'] for f in frameworks]:
+            errors = [{
+                "input_name": "framework_slug",
+                "question": "Please select a framework"
+            }]
+
+        else:
+            supplier_rows = data_api_client.export_users(framework_slug).get('users', [])
+            supplier_headers = [
+                "user_email",
+                "user_name",
+                "supplier_id",
+                "declaration_status",
+                "application_status",
+                "application_result",
+                "framework_agreement"
+            ]
+            # insert header column
+            supplier_rows.insert(0, {header: header for header in supplier_headers})
+
+            def generate():
+                for row in supplier_rows:
+                    row = ["{}".format(row.get(header, '')) for header in supplier_headers]
+                    yield '{}{}'.format(','.join(row), '\r\n')
+
+            return Response(
+                generate(),
+                mimetype='text/csv',
+                headers={
+                    "Content-Disposition": "attachment;filename=users-{}.csv".format(framework_slug),
+                    "Content-Type": "text/csv; header=present"
+                }
+            )
+
+    return render_template(
+        "export.html",
+        framework_options=framework_options,
+        errors=errors,
+        **get_template_data()
+    )
