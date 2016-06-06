2baf957dd683bd040bbbfca50dc64e552b2aed25
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-06-06 17:33:18 +0100

    Change /buyers route to use request args
    The /buyers now gets the brief_id from the request arguments, as that's
    how it's provided by the form on the index page.

--- app/main/views/buyers.py
+++ app/main/views/buyers.py
@@ -1,17 +1,17 @@
-from flask import render_template
+from flask import render_template, request
 from flask_login import login_required
 
 from .. import main
 from ... import data_api_client
 from ..auth import role_required
 
-@main.route('/buyers/<string:brief_id>', methods=['GET'])
+@main.route('/buyers', methods=['GET'])
 @login_required
 @role_required('admin')
-def find_buyer_by_brief_id(brief_id):
-    brief = data_api_client.get_brief(brief_id)
+def find_buyer_by_brief_id():
+    brief = data_api_client.get_brief(request.args.get('brief_id'))
     if brief:
-        users = brief.get('users')
+        users = brief.get('briefs').get('users')
         return render_template(
             "view_buyers.html",
             users=users
