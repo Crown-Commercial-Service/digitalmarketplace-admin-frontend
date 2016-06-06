433a8679cd8c35986e7aae217911e6920a502a23
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-06-06 12:47:18 +0100

    Create buyers view and add find by brief id route
    There is no view file for viewing buyers, so this creates one. It
    includes a route which finds a brief via the api client and renders a
    template with the associated users included.

---
+++ app/main/views/buyers.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from flask_login import login_required
+
+from .. import main
+from ... import data_api_client
+from ..auth import role_required
+
+@main.route('buyers/<string:brief_id>', methods=['GET'])
+@login_required
+@role_required('admin')
+def find_buyer_by_brief_id():
+    brief = data_api_client.get_brief(brief_id)
+    if brief:
+        users = data_api_client.get('users')
+        return render_template(
+            "view_buyers.html",
+            users=users
+        )
