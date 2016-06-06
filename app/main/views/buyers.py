95425cc7fe4c15e46de5c94b286034dfbd688407
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-06-06 13:33:22 +0100

    Register buyer views with blueprint and fix bug
    Adds a missing leading slash to the buyer route

--- app/main/views/buyers.py
+++ app/main/views/buyers.py
@@ -5,13 +5,13 @@ from .. import main
 from ... import data_api_client
 from ..auth import role_required
 
-@main.route('buyers/<string:brief_id>', methods=['GET'])
+@main.route('/buyers/<string:brief_id>', methods=['GET'])
 @login_required
 @role_required('admin')
-def find_buyer_by_brief_id():
+def find_buyer_by_brief_id(brief_id):
     brief = data_api_client.get_brief(brief_id)
     if brief:
-        users = data_api_client.get('users')
+        users = brief.get('users')
         return render_template(
             "view_buyers.html",
             users=users
