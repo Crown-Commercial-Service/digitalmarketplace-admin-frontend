baf0f7cb88a6a9d8bd19922d4a2ed0addfd39494
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-06-07 10:00:57 +0100

    Add try catch to /buyers route for bad brief id
    If the supplied brief ID didn't match a brief, a HTTPError was returned
    which took the user to a 404 page. Now the exception is caught so more
    useful information can be shown to the user.

--- app/main/views/buyers.py
+++ app/main/views/buyers.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash
 from flask_login import login_required
 
 from .. import main
@@ -9,10 +9,25 @@ from ..auth import role_required
 @login_required
 @role_required('admin')
 def find_buyer_by_brief_id():
-    brief = data_api_client.get_brief(request.args.get('brief_id'))
-    if brief:
-        users = brief.get('briefs').get('users')
+    brief_id = request.args.get('brief_id')
+
+    try:
+        brief = data_api_client.get_brief(brief_id).get('briefs')
+
+    except:
+        flash('no_brief', 'error')
         return render_template(
             "view_buyers.html",
-            users=users
-        )
+            users=list(),
+            title=None,
+            brief_id=brief_id
+        ), 404
+
+    users = brief.get('users')
+    title = brief.get('title')
+    return render_template(
+        "view_buyers.html",
+        users=users,
+        title=title,
+        brief_id=brief_id
+    )
