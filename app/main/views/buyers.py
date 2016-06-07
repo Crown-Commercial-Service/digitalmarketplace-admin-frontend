8f8d566a735f4f07d4b572f34e4f17641689dc36
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-06-07 16:03:31 +0100

    Change brief to opportunity
    The use of the word brief has been changed to opportunity to be more
    consistent with the other apps.

--- app/main/views/buyers.py
+++ app/main/views/buyers.py
@@ -9,25 +9,25 @@ from ..auth import role_required
 @main.route('/buyers', methods=['GET'])
 @login_required
 @role_required('admin')
-def find_buyer_by_brief_id():
-    brief_id = request.args.get('brief_id')
+def find_buyer_by_opportunity_id():
+    opportunity_id = request.args.get('opportunity_id')
 
     try:
-        brief = data_api_client.get_brief(brief_id).get('briefs')
+        opportunity = data_api_client.get_brief(opportunity_id).get('briefs')
 
     except:
-        flash('no_brief', 'error')
+        flash('no_opportunity', 'error')
         return render_template(
             "view_buyers.html",
             users=list(),
-            brief_id=brief_id
+            opportunity_id=opportunity_id
         ), 404
 
-    users = brief.get('users')
-    title = brief.get('title')
+    users = opportunity.get('users')
+    title = opportunity.get('title')
     return render_template(
         "view_buyers.html",
         users=users,
         title=title,
-        brief_id=brief_id
+        opportunity_id=opportunity_id
     )
--- tests/app/main/views/test_buyers.py
+++ tests/app/main/views/test_buyers.py
@@ -6,20 +6,20 @@ from lxml import html
 @mock.patch('app.main.views.buyers.data_api_client')
 class TestBuyersView(LoggedInApplicationTest):
 
-    def test_should_be_a_404_if_no_brief_found(self, data_api_client):
+    def test_should_be_a_404_if_no_opportunity_found(self, data_api_client):
         data_api_client.get_brief.return_value = None
-        response = self.client.get('admin/buyers?brief_id=1')
+        response = self.client.get('admin/buyers?opportunity_id=1')
 
         self.assertEqual(response.status_code, 404)
 
-    def test_should_display_a_useful_message_if_no_brief_found(self, data_api_client):
+    def test_should_display_a_useful_message_if_no_opportunity_found(self, data_api_client):
         data_api_client.get_brief.return_value = None
-        response = self.client.get('admin/buyers?brief_id=1')
+        response = self.client.get('admin/buyers?opportunity_id=1')
 
         document = html.fromstring(response.get_data(as_text=True))
         banner_message = document.xpath('//p[@class="banner-message"]//text()')[0].strip()
 
-        self.assertEqual("Sorry, we couldn't find a brief with the ID: 1", banner_message)
+        self.assertEqual("Sorry, we couldn't find an opportunity with the ID: 1", banner_message)
 
     def test_table_should_show_a_useful_message_if_no_users(self, data_api_client):
         data_api_client.get_brief.return_value = {
@@ -28,7 +28,7 @@ class TestBuyersView(LoggedInApplicationTest):
                 'users': list()
             }
         }
-        response = self.client.get('admin/buyers?brief_id=1')
+        response = self.client.get('admin/buyers?opportunity_id=1')
 
         document = html.fromstring(response.get_data(as_text=True))
         table_content = document.xpath('//p[@class="summary-item-no-content"]//text()')[0].strip()
@@ -36,9 +36,9 @@ class TestBuyersView(LoggedInApplicationTest):
         self.assertEqual("No buyers to show", table_content)
 
     def test_should_show_buyers_contact_details(self, data_api_client):
-        brief = self.load_example_listing("brief_response")
-        data_api_client.get_brief.return_value = brief
-        response = self.client.get('/admin/buyers?brief_id=1')
+        opportunity = self.load_example_listing("brief_response")
+        data_api_client.get_brief.return_value = opportunity
+        response = self.client.get('/admin/buyers?opportunity_id=1')
 
         document = html.fromstring(response.get_data(as_text=True))
         name = document.xpath('//td[@class="summary-item-field-first"]//text()')[1].strip()
