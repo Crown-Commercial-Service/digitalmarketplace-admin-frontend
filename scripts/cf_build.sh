a7ad94f516eafc6f474ee290fac40af0f50fa8ab
sarneaud <sarneaud@users.noreply.github.com>
2016-07-01 16:11:24 +1000

    Update buildpack
    This makes the CFLAGS hack unnecessary.

--- scripts/cf_build.sh
+++ scripts/cf_build.sh
@@ -4,9 +4,6 @@ set -eux
 
 diy-install-python
 . venv/bin/activate
-# FIXME: PyYAML tries to compile libyaml bindings if it finds a working compiler, and fails because Python headers are missing.
-# Temporary workaround is to stop the compiler from working.
-export CFLAGS="-fborkborkbork"
 pip install -r requirements.txt
 
 diy-install-node
