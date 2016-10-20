32ee9e7eff273160f3f93a1483ce3416005b3153
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2016-10-20 15:53:08 +0100

    next_agreement view: obey status filter

--- app/main/views/agreements.py
+++ app/main/views/agreements.py
@@ -62,28 +62,43 @@ def list_agreements(framework_slug):
 @login_required
 @role_required('admin', 'admin-ccs-sourcing')
 def next_agreement(supplier_id, framework_slug):
+    status = request.args.get("status")
+    if status and status not in status_labels:
+        abort(400)
+
+    # note we are NOT requesting the status-filtered supplier_framework list - we can't be sure our requested supplier
+    # will *be* in the filtered set (though it may have been at the time the url was generated) so for this view at
+    # least, any status "filtering" we do must be here in python.
     supplier_frameworks = _get_ordered_supplier_frameworks(framework_slug)
     supplier_frameworks_iter = iter(supplier_frameworks)
 
+    # first advance supplier_frameworks_iter to the requested supplier in the list, disregarding any status filter
     try:
-        while next(supplier_frameworks_iter).get("supplierId") != supplier_id:
-            pass
+        current_supplier_framework = next(
+            sf for sf in supplier_frameworks_iter if sf.get("supplierId") == supplier_id
+        )
     except StopIteration:
         # reached the end of supplier_frameworks_iter without finding an entry for supplier_id. supplier possibly
         # doesn't exist or doesn't have a signed agreement yet
         abort(404)
 
+    # now find whatever the "next" one (which satisfies any status requirement we have) is, remembering that a
+    # status_labels key might be a comma-separated list of actual API statuses
     try:
-        next_supplier_framework = next(supplier_frameworks_iter)
+        next_supplier_framework = next(
+            sf for sf in supplier_frameworks_iter if (not status) or sf.get("agreementStatus") in status.split(",")
+        )
     except StopIteration:
         # this was the last one.
         return redirect(url_for(
             '.list_agreements',
             framework_slug=framework_slug,
+            status=status,
         ))
 
     return redirect(url_for(
         '.view_signed_agreement',
         supplier_id=next_supplier_framework["supplierId"],
         framework_slug=next_supplier_framework["frameworkSlug"],
+        next_status=status,
     ))
