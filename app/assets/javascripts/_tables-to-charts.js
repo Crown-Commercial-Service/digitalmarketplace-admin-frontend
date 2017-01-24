3c072c73863316864b31cca7be762aefea7c9c92
Alex Sadleir <alex.sadleir@digital.gov.au>
2017-01-25 09:09:00 +1100

    Add chart for domain assessment metrics

--- app/assets/javascripts/_tables-to-charts.js
+++ app/assets/javascripts/_tables-to-charts.js
@@ -1,96 +1,119 @@
-(function(GOVUK, GDM) {
+(function (GOVUK, GDM) {
 
-  GOVUK.GDM.tablesToCharts = function() {
+    GOVUK.GDM.tablesToCharts = function () {
 
-    var isBigScreen = $("#framework-statistics-big-screen").length,
-        colourSequence = isBigScreen ?
-          ['#55ffee', '#ffee55', '#ff55ee', '#90ff00']
-          :
-          ['#D53880', '#2B8CC4', '#6F72AF', '#F47738'],
-        typeSequence = [
-          "area", "line", "area", "area"
-        ];
+        var isBigScreen = $("#framework-statistics-big-screen").length,
+            colourSequence = isBigScreen ?
+                ['#55ffee', '#ffee55', '#ff55ee', '#90ff00']
+                :
+                ['#D53880', '#2B8CC4', '#6F72AF', '#F47738'],
+            typeSequence = [
+                "area", "area", "bar"
+            ];
 
-    $(".framework-statistics table")
-      .each(function(tableIndex) {
+        $(".framework-statistics table")
+            .each(function (tableIndex) {
+                var chartConfig = {
+                    bindto: '#chart-' + tableIndex,
+                    legend: {
+                        show: false
+                    },
+                    color: {
+                        pattern: colourSequence
+                    },
+                    size: {
+                        height: isBigScreen ? 320 : 480
+                    },
+                    tooltip: {
+                        show: !isBigScreen
+                    }
+                };
+                var columns = [], groups = [], types = {}, chartType = '';
 
-        var columns = [], groups = [], types = {};
+                $("thead th", this).each(function (colIndex) {
 
-        $("thead th", this).each(function(colIndex) {
+                    var columnHeading = $.trim($(this).text());
 
-          var columnHeading = $.trim($(this).text()),
-              chartType = typeSequence[tableIndex];
+                    chartType = typeSequence[tableIndex];
 
-          $(this).append("<span class='key' />");
+                    $(this).append("<span class='key' />");
 
-          $(".key", this).css(
-            "background", colourSequence[colIndex - 1]
-          );
+                    $(".key", this).css(
+                        "background", colourSequence[colIndex - 1]
+                    );
 
-          columns.push([columnHeading]);
+                    columns.push([columnHeading]);
 
-          if (colIndex) {
-            if ("area" == chartType) {
-              groups.push(columnHeading);
-            }
-            types[columnHeading] = chartType;
-          }
+                    if (colIndex) {
+                        if ("area" == chartType) {
+                            groups.push(columnHeading);
+                        }
+                        if ("bar" == chartType && colIndex != 0) {
+                            groups.push(columnHeading);
+                        }
+                        types[columnHeading] = chartType;
+                    }
 
-        });
+                });
 
-        $("tbody tr", this).each(function(rowIndex) {
+                $(this).before($("<div class='statistics-chart' id='chart-" + tableIndex + "'/>"));
 
-          $("td", this).each(function(colIndex) {
+                $("tbody tr", this).each(function (rowIndex) {
 
-            columns[colIndex].push(
-              $.trim($(this).text())
-            );
+                    $("td", this).each(function (colIndex) {
+                        if ("bar" == chartType && colIndex == 0) {
+                            groups.push($.trim($(this).text()));
+                        }
+                        columns[colIndex].push(
+                            $.trim($(this).text())
+                        );
 
-          });
+                    });
 
-        });
+                });
 
-        $(this).before($("<div class='statistics-chart' id='chart-" + tableIndex + "'/>"));
+                if (chartType == 'bar') {
+                    console.log(groups);
+                    chartConfig['data'] = {
+                        columns: columns,
+                        groups: [groups],
+                        type: 'bar'
+                    };
+                    chartConfig['axis'] = {
+                        x: {
+                            type: 'category',
+                            categories: groups
 
-        var chart = c3.generate({
-            bindto: '#chart-' + tableIndex,
-            data: {
-              x: "Date and time",
-              xFormat: "%Y/%m/%d %H:%M:%S",
-              columns: columns,
-              order: null,
-              types: types,
-              groups: [groups],
-            },
-            axis: {
-              x: {
-                type: 'timeseries',
-                tick: {
-                  format: '%Y-%m-%d %H:%M:%S',
-                  fit: false
-                },
-                show: false
-              },
-              y: {
-                show: false
-              }
-            },
-            legend: {
-              show: false
-            },
-            color: {
-              pattern: colourSequence
-            },
-            size: {
-              height: isBigScreen ? 320 : 480
-            },
-            tooltip: {
-              show: !isBigScreen
-            }
-        });
+                        }
+                    };
+                    var chart = c3.generate(chartConfig);
+                } else {
 
-      });
+                    chartConfig['data'] = {
+                        x: "Date and time",
+                        xFormat: "%Y/%m/%d %H:%M:%S",
+                        columns: columns,
+                        order: null,
+                        types: types,
+                        groups: [groups],
+                    };
+                    chartConfig['axis'] = {
+                        x: {
+                            type: 'timeseries',
+                            tick: {
+                                format: '%Y-%m-%d %H:%M:%S',
+                                fit: false
+                            },
+                            show: false
+                        },
+                        y: {
+                            show: false
+                        }
+                    };
+                    var chart = c3.generate(chartConfig);
+                }
+                ;
+            });
+    };
 
-  };
-
-}).apply(this, [GOVUK||{}, GOVUK.GDM||{}]);
+}).apply(this, [GOVUK || {}, GOVUK.GDM || {}]);
