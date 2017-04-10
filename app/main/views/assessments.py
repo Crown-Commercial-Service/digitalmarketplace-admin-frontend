af5e88389cf31e789b9d5bcfdb880837fe21cdb7
matt-sm <mattysmith@gmail.com>
2017-04-10 15:21:26 +1000

    approve assessment

--- app/main/views/assessments.py
+++ app/main/views/assessments.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, jsonify, url_for
 from flask_login import login_required
 
 from .. import main
@@ -12,12 +12,14 @@ from react.render import render_component
 @login_required
 @role_required('admin')
 def assessments_review():
+    SCHEME = request.environ['wsgi.url_scheme']
     assessments = data_api_client.req.assessments().get()['assessments']
 
     rendered_component = render_component(
         'bundles/ApplicationsAdmin/AssessmentsAdminWidget.js',
         {
             'assessments': assessments,
+            'meta': {'url_approve': url_for('main.assessments_approve', _external=True, _scheme=SCHEME)}
         }
     )
 
@@ -25,3 +27,14 @@ def assessments_review():
         '_react.html',
         component=rendered_component
     )
+
+
+@main.route('/assessments/approve', methods=['POST'])
+@login_required
+@role_required('admin')
+def assessments_approve():
+    id = request.get_json(force=True)['id']
+    assessment = data_api_client.req.assessments(id).get()
+    result = data_api_client.req.suppliers(assessment['supplier_domain']['supplier']['id']) \
+        .domains(assessment['supplier_domain']['domain']['id']).assessed().post({})
+    return jsonify(result)
