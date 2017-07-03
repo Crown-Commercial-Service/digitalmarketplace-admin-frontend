fafd1bd9d26d40f962320a945fb1f0f85f1a4e86
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2017-07-03 17:56:15 +0100

    default.nix: add more useful $PS1

--- default.nix
+++ default.nix
@@ -8,8 +8,9 @@ let
     localOverridesPath = ./local.nix;
   } // argsOuter;
 in (with args; {
-  digitalMarketplaceAdminFrontendEnv = (pkgs.stdenv.mkDerivation {
+  digitalMarketplaceAdminFrontendEnv = (pkgs.stdenv.mkDerivation rec {
     name = "digitalmarketplace-admin-frontend-env";
+    shortName = "dm-adm-fe";
     buildInputs = [
       pythonPackages.virtualenv
       pkgs.nodejs
@@ -42,6 +43,8 @@ in (with args; {
     LANG="en_GB.UTF-8";
 
     shellHook = ''
+      export PS1="\[\e[0;34m\](nix-shell\[\e[0m\]:\[\e[0;34m\]${shortName})\[\e[0;32m\]\u@\h\[\e[0m\]:\[\e[0m\]\[\e[0;34m\]\w\[\e[0m\]\$ "
+
       if [ ! -e $VIRTUALENV_ROOT ]; then
         ${pythonPackages.virtualenv}/bin/virtualenv $VIRTUALENV_ROOT
       fi
