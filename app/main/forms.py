7d4e8d35435ec929cc925979dd87dd5ba9fe2564
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2017-07-16 14:32:18 +0100

    Remove unused ServiceUpdateAuditEventsForm WTForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,48 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField, DateField, PasswordField, IntegerField
-from wtforms.validators import Optional, AnyOf, DataRequired, \
-    Email
-from datetime import datetime
-from dmutils.formats import DATE_FORMAT
+from wtforms import PasswordField
+from wtforms.validators import DataRequired, Email
 from dmutils.forms import StripWhitespaceStringField
-
-
-class ServiceUpdateAuditEventsForm(Form):
-    audit_date = DateField(
-        'Audit Date',
-        format=DATE_FORMAT,
-        validators=[Optional()])
-    acknowledged = StringField(
-        'acknowledged',
-        default="false",
-        validators=[
-            AnyOf(['all', 'true', 'false']),
-            Optional()]
-    )
-    page = IntegerField(
-        default=1,
-        validators=[
-            Optional()
-        ]
-    )
-
-    def default_acknowledged(self):
-        if self.acknowledged.data:
-            return self.acknowledged.data
-        else:
-            return self.acknowledged.default
-
-    def format_date(self):
-        if self.audit_date.data:
-            return datetime.strftime(self.audit_date.data, DATE_FORMAT)
-        else:
-            return None
-
-    def format_date_for_display(self):
-        if self.audit_date.data:
-            return self.audit_date.data
-        else:
-            return datetime.utcnow()
 
 
 class LoginForm(Form):
