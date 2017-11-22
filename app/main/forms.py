ba4c2dc22aae5dd4f862aad7c388eecf36acfbd8
benvand <benjaminfvandersteen@gmail.com>
2017-11-22 15:37:47 +0000

    Change import to indicate function of imported classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,23 +1,24 @@
 from flask_wtf import Form
-from wtforms.validators import DataRequired, Email
+from wtforms import validators
+
 from dmutils.forms import StripWhitespaceStringField
 
 
 class EmailAddressForm(Form):
     email_address = StripWhitespaceStringField('Email address', validators=[
-        DataRequired(message="Email can not be empty"),
-        Email(message="Please enter a valid email address")
+        validators.DataRequired(message="Email can not be empty"),
+        validators.Email(message="Please enter a valid email address")
     ])
 
 
 class MoveUserForm(Form):
     user_to_move_email_address = StripWhitespaceStringField('Move an existing user to this supplier', validators=[
-        DataRequired(message="Email can not be empty"),
-        Email(message="Please enter a valid email address")
+        validators.DataRequired(message="Email can not be empty"),
+        validators.Email(message="Please enter a valid email address")
     ])
 
 
 class EmailDomainForm(Form):
     new_buyer_domain = StripWhitespaceStringField('Add a buyer email domain', validators=[
-        DataRequired(message="The domain field can not be empty.")
+        validators.DataRequired(message="The domain field can not be empty.")
     ])
