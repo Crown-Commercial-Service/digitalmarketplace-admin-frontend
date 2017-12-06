7a495b297cc447e16be39d80f146bfdc62c62ce8
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2017-12-06 14:50:21 +0000

    Make index page link to find buyers by opportunity ID
    Removes duplicate search form from index page and replaces it with a link.

--- tests/app/main/views/test_index_page.py
+++ tests/app/main/views/test_index_page.py
@@ -185,3 +185,20 @@ class TestIndex(LoggedInApplicationTest):
         assert link_is_visible is link_should_be_visible, (
             "Role {} {} see the link".format(role, "can not" if link_should_be_visible else "can")
         )
+
+    @pytest.mark.parametrize("role, link_should_be_visible", [
+        ("admin", True),
+        ("admin-ccs-category", True),
+        ("admin-ccs-sourcing", False),
+        ("admin-framework-manager", False),
+        ("admin-manager", False),
+    ])
+    def test_find_buyer_by_opportunity_id_link_is_shown_to_users_with_right_roles(self, role, link_should_be_visible):
+        self.user_role = role
+        response = self.client.get('/admin')
+        document = html.fromstring(response.get_data(as_text=True))
+        link_is_visible = bool(document.xpath('.//a[@href="/admin/buyers"]'))
+
+        assert link_is_visible is link_should_be_visible, (
+            "Role {} {} see the link".format(role, "can not" if link_should_be_visible else "can")
+        )
