348964dbd00632554cb3db31279eb812166fda09
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-12-08 10:24:13 +0000

    Make the page heading role-specific

--- tests/app/main/views/test_search.py
+++ tests/app/main/views/test_search.py
@@ -20,15 +20,22 @@ class TestSearchView(LoggedInApplicationTest):
         actual_code = response.status_code
         assert actual_code == expected_code, "Unexpected response {} for role {}".format(actual_code, role)
 
-    def test_should_show_find_suppliers_and_services_page(self):
+    @pytest.mark.parametrize("role, expected_header", [
+        ("admin", "Edit supplier accounts or view services"),
+        ("admin-ccs-category", "Edit suppliers and services"),
+        ("admin-ccs-sourcing", "Edit supplier declarations"),
+        ("admin-framework-manager", "View suppliers and services"),
+    ])
+    def test_should_show_find_suppliers_and_services_page_with_role_appropriate_header(self, role, expected_header):
+        self.user_role = role
         response = self.client.get('/admin/find-suppliers-and-services')
         page_html = response.get_data(as_text=True)
         document = html.fromstring(page_html)
-        heading = document.xpath(
+        header = document.xpath(
             '//header[@class="page-heading page-heading-without-breadcrumb"]//h1/text()')[0].strip()
 
         assert response.status_code == 200
-        assert heading == "Find suppliers and services"
+        assert header == expected_header
 
     @pytest.mark.parametrize("role,form_1_exists,form_2_exists,form_3_exists", [
         ("admin", True, True, True),
