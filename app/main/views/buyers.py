eab53fb41b2e58ceaa9231b7a7518d0312c822d0
anthonymckerron <47020580+anthonymckerron@users.noreply.github.com>
2019-03-19 14:35:07 +1100

    [MAR-2807] (#100)
    * Adding functions/html for the adding/removing and displaying for RFXs.
    
    * Changes to satisfy Circle CI.
    
    * Second update for check styles.
    
    * Final style change for Circle CI.
    
    * Style Change for CCI
    
    * Circle CI Style Check
    
    * Style Check change for CCI
    
    * CircleCI StyleCheck #666
    
    * Fix Tests
    
    * Adding formatting changes as per recommendations
    
    * Removing unnecessary if statements.
    
    * Update app/templates/view_buyers.html
    
    Co-Authored-By: anthonymckerron <47020580+anthonymckerron@users.noreply.github.com>
    
    * Update app/templates/view_buyers.html
    
    Co-Authored-By: anthonymckerron <47020580+anthonymckerron@users.noreply.github.com>

--- app/main/views/buyers.py
+++ app/main/views/buyers.py
@@ -113,6 +113,35 @@ def update_brief(brief_id):
                                   'applications_closed_at': request.form['closed_at']
                                   },
                        'update_details': {'updated_by': current_user.email_address}}).get('briefs')
+        elif request.form.get('add_seller_to_rfx_brief'):
+            brief = (
+                data_api_client
+                .req
+                .briefs(brief_id)
+                .suppliers(
+                    request.form['add_seller_to_rfx_brief'].strip()
+                )
+                .put({
+                    'update_details': {
+                        'updated_by': current_user.email_address
+                    }
+                })
+                .get('briefs')
+            )
+        elif request.form.get('remove_seller_from_rfx_brief'):
+            brief = (
+                data_api_client
+                .req
+                .briefs(brief_id)
+                .suppliers(
+                    request.form['remove_seller_from_rfx_brief'].strip()
+                )
+                .delete({
+                    'update_details': {
+                        'updated_by': current_user.email_address
+                    }
+                })
+                .get('briefs'))
         elif 'edit_seller_email_list' in request.form:
             edit_seller_email_list = request.form.get('edit_seller_email_list', []).split(NEW_LINE)
             brief = data_api_client.req.briefs(brief_id).admin() \
@@ -136,6 +165,8 @@ def update_brief(brief_id):
                 .post({'briefs': {'areaOfExpertise': request.form['edit_area_of_expertise']
                                   },
                        'update_details': {'updated_by': current_user.email_address}}).get('briefs')
+        else:
+            brief = data_api_client.get_brief(brief_id).get('briefs')
 
     except HTTPError, e:
         flash(e.message, 'error')
