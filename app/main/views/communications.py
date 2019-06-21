f886ec4e399ceeb700c7b84076f1be9b6a5fd858
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-06-21 10:02:43 +0100

    add delete_communication view & "confirm" template
    will be linked to from new "manage communications" page

--- app/main/views/communications.py
+++ app/main/views/communications.py
@@ -1,11 +1,23 @@
+from pathlib import PurePath
+
 from dmutils import s3  # this style of import so we only have to mock once
 from dmutils.documents import file_is_pdf, file_is_csv, file_is_open_document_format
 from dmutils.flask import timed_render_template as render_template
-from flask import redirect, url_for, current_app, request, flash
+from flask import redirect, url_for, current_app, request, flash, abort
 
 from .. import main
 from ..auth import role_required
 from ... import data_api_client
+from ..helpers.frameworks import get_framework_or_404
+
+
+def _get_comm_type_root(framework_slug, comm_type):
+    # PurePath has the advantage that it doesn't handle ".." elements specially, and so we're safe from a whole
+    # category of tricks
+    return PurePath(framework_slug) / "communications" / "updates" / f"{comm_type}s"
+
+
+_comm_types = ("communication", "clarification",)
 
 
 @main.route('/communications/<framework_slug>', methods=['GET'])
@@ -65,3 +77,36 @@ def upload_communication(framework_slug):
             flash('New clarification was uploaded.')
 
     return redirect(url_for('.manage_communications', framework_slug=framework_slug))
+
+
+@main.route('/communications/<framework_slug>/delete/<string:comm_type>/<path:filepath>', methods=("GET", "POST",))
+@role_required('admin-framework-manager')
+def delete_communication(framework_slug, comm_type, filepath):
+    if comm_type not in _comm_types:
+        abort(404)
+
+    framework = get_framework_or_404(data_api_client, framework_slug)
+
+    if request.method == "POST":
+        if "confirm" not in request.form:
+            abort(400, "Expected 'confirm' parameter in POST request")
+
+        communications_bucket = s3.S3(current_app.config['DM_COMMUNICATIONS_BUCKET'])
+        full_path = _get_comm_type_root(framework_slug, comm_type) / filepath
+
+        # do this check ourselves - deleting an object in S3 silently has no effect, forwarding this behaviour to the
+        # user is a confusing thing to do
+        if not communications_bucket.path_exists(str(full_path)):
+            abort(404, f"{filepath} not present in S3 bucket")
+
+        communications_bucket.delete_key(str(full_path))
+
+        flash(f"{comm_type.capitalize()} ‘{filepath}’ was deleted for {framework['name']}.")
+        return redirect(url_for('.manage_communications', framework_slug=framework_slug))
+
+    return render_template(
+        'confirm_communications_deletion.html',
+        framework=framework,
+        comm_type=comm_type,
+        filepath=filepath,
+    )
