07ad10499d2114ec5fa5d4802879337ad877ca75
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-09-19 14:11:47 +0100

    Use Phasebanner instead of user research sign up banner
    While we were trying to migrate from govuk_template to govuk-frontend
    template. An issue was identified with having the user research sign up
    banner positioned between the top black header and the next red horizontal
    line. It would mean that we could not use GOV.UK Frontend header component
    and we would still have to maintain the user research banner and it also
    relied on govuk_templates cookie implementation which we would also need
    to either copy or remove completely. As this was the only component using
    GOVUK.cookie method it would be good to remove it.
    
    This change also brings our designs closer inline with GOV.UK Design system
    
    Stefan Nicolaou, Kat Stevens and I, meet and agreed that we would drop
    the user research banner and instead take all users to the "help" page.
    On the "Help" page we would add some copy for users to sign up to user
    research. It will still require users to have an existing account before
    they can sign up for user research.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require ../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/shim-links-with-button-role.js
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/shim-links-with-button-role.js
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/show-hide-content.js
-//= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/user-research-consent-banner.js
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/report-a-problem.js
 //= require ../../../node_modules/govuk-frontend/all.js
 //= require _scroll-through-statistics.js
