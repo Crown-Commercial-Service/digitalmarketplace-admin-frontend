9b4115f6b37a556f3d6039f9ed3a1299eb32bbc6
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-10-15 11:26:07 +0100

    Remove Flask-Script manager from application.py
    With built in Flask CLI this is no longer needed.

--- application.py
+++ application.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python
-
 import os
+
 from app import create_app
-from dmutils import init_manager
 
-application = create_app(
-    os.getenv('DM_ENVIRONMENT') or 'development'
-)
-manager = init_manager(application, 5004, ['./app/content/frameworks'])
 
-if __name__ == '__main__':
-    manager.run()
+application = create_app(os.getenv("DM_ENVIRONMENT") or "development")
