44533ce342a751e7aae1b0549b2d088597cf57c4
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-04-20 18:08:34 +0100

    Show last two expired G-Cloud frameworks
    Admins still need to handle requests for old G-Cloud frameworks. Make this easier for them by including links for the last two expired G-Cloud frameworks on the homepage.
    
    There was a deliberate decision to exclude old G-Cloud frameworks made in 2017. However, I can find nothing that says why the change was made. So I think we're probably safe enough to reverse that decision, given that CCS reported the current behaviour as a bug.

--- app/main/helpers/service.py
+++ app/main/helpers/service.py
@@ -2,6 +2,29 @@ import datetime
 import re
 
 
+def filter_relevant_frameworks(frameworks):
+    """
+    Show admins only the frameworks that they're likely to be interested in.
+
+    Include all live frameworks. Include all expired DOS frameworks, but only the last 2 expired G-Cloud frameworks.
+    """
+    frameworks.sort(key=lambda x: x['id'], reverse=True)
+
+    expired_gcloud_frameworks = 0
+
+    for framework in frameworks:
+        if framework['status'] == 'coming':
+            continue
+
+        if framework["family"] == 'digital-outcomes-and-specialists':
+            yield framework
+        elif framework['status'] != 'expired':
+            yield framework
+        elif framework['status'] == 'expired' and expired_gcloud_frameworks < 2:
+            expired_gcloud_frameworks += 1
+            yield framework
+
+
 def parse_document_upload_time(data):
     match = re.search(r"(\d{4}-\d{2}-\d{2}-\d{2}\d{2})\..{2,3}$", data)
     if match:
